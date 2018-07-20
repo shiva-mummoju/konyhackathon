c1c617b3d3b340206315dd084c6277397a304386
shivasairam101 <shiva.sairam97@gmail.com>
2018-07-20 21:47:53 +0530

    some js code

--- modules/postHistorySegmentClicked.js
+++ modules/postHistorySegmentClicked.js
@@ -1,8 +1,21 @@
 //Type your code here
 
 
-function onPostRowClicked(){
+function onPostRowClicked(eventObject,sectionNumber,rowNumber){
 	
-	
+	 var selectedObj = frmPostHistory.flxPosts.segPosts.selectedRowItems[0];
+  var postTitle = selectedObj.lblPostTitle;
+  var postProduct = selectedObj.lblPostProducts;
+  var postImg = selectedObj.imgPosts;
+  var shareCount = selectedObj.PostShareCount;
+  
+  
+  frmPostSpecific.lblPostTitle.text  = postTitle;
+  frmPostSpecific.lblPostInfo.text = "This is a really nice product from Amway!";
+  frmPostSpecific.lblShareCount.text = shareCount;
+  frmPostSpecific.lblLikeCount.text = "10";
+  frmPostSpecific.lblViewCount.txt = "20";
+  
+  frmPostSpecific.show();
 
 }
