ca66b91be46dccf2c619642fb3fd96055ad53ff2
sandilya12 <saisandilya22@gmail.com>
2018-07-20 21:02:25 +0530

    login and camera

---
+++ studioactions/mobile/AS_Button_f0cc09e6d85b47a1aea3c8ba97e7bcf6.js
@@ -0,0 +1,3 @@
+function AS_Button_f0cc09e6d85b47a1aea3c8ba97e7bcf6(eventobject) {
+    return openMediaGallery.call(this);
+}
---
+++ studioactions/mobile/AS_Button_gd0beaa567a9426c9d15f717209191fa.js
@@ -0,0 +1,3 @@
+function AS_Button_gd0beaa567a9426c9d15f717209191fa(eventobject) {
+    return openMediaGallery.call(this);
+}
