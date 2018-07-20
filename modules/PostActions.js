8d970185ea9f3a21dfcc0852055e8f4490233f6e
tabhi0797 <abhinav071197@gmail.com>
2018-07-20 21:07:00 +0530

    post http call

---
+++ modules/PostActions.js
@@ -0,0 +1,28 @@
+//Type your code here
+
+//var httpclient = null;
+function postToFacebook()
+{
+  	 httpclient = new kony.net.HttpRequest();
+ 	
+	httpclient.open(constants.HTTP_METHOD_GET, "https://images.contentstack.io/v3/assets/blte699a6163cd8c122/bltf386045334d778f1/5ac327fd23d67a790b465814/download" );
+  httpclient.setRequestHeader("Content-type","application/json");
+
+    httpclient.setRequestHeader("Access-Control-Allow-Origin","*");
+httpclient.onReadyStateChange = fnCallBack
+
+  httpclient.send();
+// 	var rb = httpclient.response;
+  
+//   alert(rb);
+}
+
+function fnCallBack()
+{
+  
+  
+   if(httpclient.readyState== constants.HTTP_READY_STATE_DONE)
+     alert(JSON.stringify(httpclient));
+    
+}
+
---
+++ studioactions/mobile/actionPostHttpCall.js
@@ -0,0 +1,7 @@
+function actionPostHttpCall(eventobject) {
+    return AS_Button_d8157c6d4fd6498db482bb702166e627(eventobject);
+}
+
+function AS_Button_d8157c6d4fd6498db482bb702166e627(eventobject) {
+    return postToFacebook.call(this);
+}
